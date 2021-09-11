3caf0535184954a38c1fd59e52fb50a5978b519b
dangerdrennan <pdrennan90@gmail.com>
2021-09-10 21:09:39 -0400

    initial backend up

---
+++ backend/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+const app = express();
+import routes from './routes';
+import cors from 'cors';
+
+app.use(cors({origin: 'http://localhost:4200'}))
+
+app.listen(4201, "localhost", function(){
+    console.log('Server now listening on 4201');
+})
+
+app.use(express.json());
+app.use(routes);
--- database/create_admin.sql
+++ database/create_admin.sql
@@ -6,4 +6,3 @@ CREATE USER cams WITH PASSWORD 'pswd' CREATEDB;
 GRANT pg_read_server_files, pg_write_server_files TO cams;
 
 CREATE DATABASE cams WITH OWNER = cams;
--- \c cams
