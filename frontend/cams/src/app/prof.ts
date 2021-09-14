898e1d5de86171391c1f656adaa46014ed773ea9
dangerdrennan <pdrennan90@gmail.com>
2021-09-14 01:43:23 -0400

    added prof and auth interfaces

---
+++ frontend/cams/src/app/prof.ts
@@ -0,0 +1,8 @@
+export interface Professor {
+    prof_id: number,
+    email: string,
+    f_name: string,
+    l_name: string,
+    department: string,
+    is_admin: boolean
+}
--- frontend/cams/src/app/services/login.service.ts
+++ frontend/cams/src/app/services/login.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Auth } from '../Auth'
+
+
+
+
+const httpOptions =
+{
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
+  private apiURL = 'http://localhost:4201/'
+  auth:Auth ={
+    email: '',
+    password: ''
+  };
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+
+  // Auth(): Observable<Auth[]>{
+  //   return this.http.get<Auth[]>(this.apiURL +'login');
+  // }
+
+
+
 }
