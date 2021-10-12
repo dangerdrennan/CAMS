f96ee48d82bf1dcf74cb9c4907ab1e9cbade42af
Jessica Huezo <53642436+JessicaHuezo@users.noreply.github.com>
2021-10-12 13:39:10 -0800

    made prof dash responsive

--- frontend/cams/src/app/shared/sidebar/sidebar.component.ts
+++ frontend/cams/src/app/shared/sidebar/sidebar.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { LoginService } from '../../services/login.service';
 import { AuthService } from '@auth0/auth0-angular';
 import { DOCUMENT } from '@angular/common';
+import { ProfDashboardService } from 'src/app/services/prof-dashboard.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -10,8 +11,8 @@ import { DOCUMENT } from '@angular/common';
 })
 export class SidebarComponent {
 
-  public isAssessing: boolean = true;
-  constructor(public loginService: LoginService,
+
+  constructor(public profDashService: ProfDashboardService, public loginService: LoginService,
     public auth: AuthService,
     @Inject(DOCUMENT) private doc: Document) { }
 
