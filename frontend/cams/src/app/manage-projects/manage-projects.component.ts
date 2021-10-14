c2c0cfdb2259d5bc3cb9eb023b0028c781a9c490
Jessica Huezo <53642436+JessicaHuezo@users.noreply.github.com>
2021-10-14 14:46:49 -0800

    added form for managing projects

--- frontend/cams/src/app/manage-projects/manage-projects.component.ts
+++ frontend/cams/src/app/manage-projects/manage-projects.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,14 +8,37 @@ import { Router } from '@angular/router';
   styleUrls: ['./manage-projects.component.css']
 })
 export class ManageProjectsComponent implements OnInit {
-
-  constructor(private router: Router) { }
+  form!: FormGroup;
+  form2!: FormGroup;
+  constructor(private router: Router, private builder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.form = this.builder.group({
+      projectName: ['', Validators.required],
+      studentName: ['', Validators.required],
+      term: ['', Validators.required],
+      year: ['', Validators.required],
+      degree: ['', Validators.required],
+      status: ['Not Graded'] // by default we'll mark as not graded
+    })
+    this.form2 = this.builder.group({
+      profName: ['', Validators.required]
+    })
   }
 
   goBack() {
     this.router.navigateByUrl('/home');
+  }
+
+  submit() {
+    this.form.reset({
+      term: '',
+      year: ''
+    });
+  }
+
+  secondSubmit() {
+    this.form2.reset();
   }
 
 }
