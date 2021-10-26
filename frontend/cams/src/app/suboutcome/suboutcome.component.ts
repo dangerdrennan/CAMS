3ee1131512cd63030d566b437dbd528837436813
dangerdrennan <pdrennan90@gmail.com>
2021-10-26 14:15:39 -0400

    score rows are highlighted with radio button logic

--- frontend/cams/src/app/suboutcome/suboutcome.component.ts
+++ frontend/cams/src/app/suboutcome/suboutcome.component.ts
@@ -20,6 +20,42 @@ export class SuboutcomeComponent implements OnInit {
     this.subDeets$ = this.assessmentService.getCSSuboutcomes(this.outcome_cat)
     
    }
+
+   recordGrade(score_id: string, grade: number){
+    console.log(score_id, grade)
+    const poor_box = document.getElementById(score_id+'_'+'poor');
+    const developing_box = document.getElementById(score_id+'_'+'developing');
+    const satisfactory_box = document.getElementById(score_id+'_'+'satisfactory');
+    const excellent_box = document.getElementById(score_id+'_'+'excellent');
+    switch(grade) {
+      case 1:
+        poor_box.className = "scoreable selected"
+        developing_box.className = "scoreable"
+        satisfactory_box.className = "scoreable"
+        excellent_box.className = "scoreable"
+        break;
+      case 2:
+        poor_box.className = "scoreable"
+        developing_box.className = "scoreable selected"
+        satisfactory_box.className = "scoreable"
+        excellent_box.className = "scoreable"
+        break;
+      case 3:
+        poor_box.className = "scoreable"
+        developing_box.className = "scoreable"
+        satisfactory_box.className = "scoreable selected"
+        excellent_box.className = "scoreable"
+      break;
+      case 4:
+        poor_box.className = "scoreable"
+        developing_box.className = "scoreable"
+        satisfactory_box.className = "scoreable"
+        excellent_box.className = "scoreable selected"
+      break;
+      default:
+        // code block
+    }
+   }
   
   ngOnInit(): void {
     console.log("should not be undefined: " + this.outcome_cat)
