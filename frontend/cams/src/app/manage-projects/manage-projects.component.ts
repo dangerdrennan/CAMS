cc2d94997bdf76d05ed34d845ef353b2a4ac08f2
Jessica Huezo <53642436+JessicaHuezo@users.noreply.github.com>
2021-11-02 13:06:24 -0800

    manage project ui

--- frontend/cams/src/app/manage-projects/manage-projects.component.ts
+++ frontend/cams/src/app/manage-projects/manage-projects.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Project } from '../Project';
+import { ProjectService } from '../services/project.service';
+import { Student } from '../Student';
 
 @Component({
   selector: 'app-manage-projects',
@@ -8,37 +11,118 @@ import { Router } from '@angular/router';
   styleUrls: ['./manage-projects.component.css']
 })
 export class ManageProjectsComponent implements OnInit {
-  form!: FormGroup;
-  form2!: FormGroup;
-  constructor(private router: Router, private builder: FormBuilder) { }
+  allProj:Project[] = []
+  allStud: Student[] = []
+  addProjForm!: FormGroup;
+  addStudForm!: FormGroup;
+
+  project!: Project
+  student!: Student
+
+  projIndex: number = 0;
+  termId!: number
+  projId:number | undefined
+  constructor(private builder: FormBuilder, private projectService: ProjectService) { }
 
   ngOnInit(): void {
-    this.form = this.builder.group({
+    // initialize project form
+    this.addProjForm = this.builder.group({
       projectName: ['', Validators.required],
-      studentName: ['', Validators.required],
-      term: ['', Validators.required],
-      year: ['', Validators.required],
+    })
+
+    // initialize student form
+    this.addStudForm = this.builder.group({
+      studentFirst: ['', Validators.required],
+      studentLast: ['', Validators.required],
       degree: ['', Validators.required],
       status: ['Not Graded'] // by default we'll mark as not graded
     })
-    this.form2 = this.builder.group({
-      profName: ['', Validators.required]
+
+    // initially display all projects
+    this.allProjects()
+
+
+    console.log("all students", this.allStud)
+
+
+  }
+
+  // display all projects
+  allProjects() {
+    return this.projectService.getAllProjects().subscribe((res) => {
+      res.filter((item: Project) => {
+        this.projectService.getProjectStudents(item).subscribe((res) => {
+          res.filter((stud: Student) => {
+
+            this.allStud.push(stud)
+            this.generateAssessments(stud)
+          })
+        })
+         this.allProj.push(item)
+      })
     })
   }
 
-  goBack() {
-    this.router.navigateByUrl('/home');
+  // add new project
+  submitAddNewProj(projectName: string) {
+    this.project = {
+      title: this.addProjForm.get("title")?.value
+    }
+
+    this.projectService.addProject(projectName).subscribe((res) => {
+      res.filter((item: Project) => {
+        this.allProj.push(item)
+        return this.addStudToProj(item.proj_id!, item.term_id!)
+      })
+    })
   }
 
-  submit() {
-    this.form.reset({
-      term: '',
-      year: ''
-    });
+  // link student to project
+  addStudToProj(projId: number, termId: number) {
+    // populate student object with form input from user
+    this.student = {
+      f_name: this.addStudForm.get("studentFirst")?.value,
+      l_name: this.addStudForm.get("studentLast")?.value,
+      degree: this.addStudForm.get("degree")?.value,
+      proj_id: projId,
+      term_id: termId
+    }
+    this.projectService.assignStudentToProject(this.student).subscribe(() => {
+      this.allStud.push(this.student)
+      this.generateAssessments(this.student)
+      location.reload()
+    })
   }
 
-  secondSubmit() {
-    this.form2.reset();
+  // delete an entire project
+  deleteProject(project: Project) {
+    this.projectService.deleteProject(project.title).subscribe(() => {
+      location.reload()
+    })
   }
+
+
+  // get the project id when edit button clicked to trigger modal
+  editButton(project: Project) {
+    return this.projIndex = project.proj_id!
+  }
+
+  // trigger to add another student to a project in the modal
+  addStudInModal(project: Project) {
+    this.addStudToProj(project.proj_id!, project.term_id!)
+  }
+
+  // trigger to delete a student from a project in the modal
+  deleteStudFromProjInModal(student: Student) {
+    this.projectService.deleteStudent(student.student_id!).subscribe(() => {
+      location.reload()
+    })
+  }
+
+  // add assessments for each student
+  generateAssessments(stud: Student) {
+    this.projectService.addAssessment(stud).subscribe()
+  }
+
 
 }
