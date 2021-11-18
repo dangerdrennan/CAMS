70cfae560cab64f3cda666083ecbe139b17f0a97
dangerdrennan <pdrennan90@gmail.com>
2021-11-18 02:48:32 -0500

    new db files for dummy data

--- database/results_functions/test.sql
+++ database/results_functions/test.sql
@@ -1,1 +1,21 @@
-Select outcome_cats_cs, suboutcome_details_cs from sem_req inner join suboutcome_details_cs on suboutcomes_details_cs.outcome_cat_id;
+SELECT
+        p.title,
+        s.f_name,
+        s.l_name,
+        t.semester,
+        t.year,
+        a.graded,
+        a.assessment_id,
+        a.degree
+        FROM
+            prof pr
+        INNER JOIN assessment a 
+            ON pr.prof_email = a.prof_email
+        INNER JOIN student s 
+            ON s.student_id = a.student_id
+        INNER JOIN project p
+            ON s.proj_id = p.proj_id
+        INNER JOIN term t
+            ON t.term_id = a.term_id
+        WHERE
+            pr.prof_email = 'ginandjuice' and p.term_id = get_current_term()`
