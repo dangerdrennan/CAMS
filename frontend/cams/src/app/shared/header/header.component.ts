0dea835cf90f5e82bbd37dd6533f9db2f4fbe1d8
Jessica Huezo <53642436+JessicaHuezo@users.noreply.github.com>
2021-11-21 21:29:18 -0900

    sidebar and header update to change dynamically

--- frontend/cams/src/app/shared/header/header.component.ts
+++ frontend/cams/src/app/shared/header/header.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { take } from 'rxjs/operators';
-import { Professor } from 'src/app/prof';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HeaderService } from 'src/app/services/header.service';
 import { LoginService } from 'src/app/services/login.service';
 import { ProfDashboardService } from 'src/app/services/prof-dashboard.service';
 
@@ -10,8 +10,10 @@ import { ProfDashboardService } from 'src/app/services/prof-dashboard.service';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  @Input() title: string = ''
 
-  constructor(public loginService: LoginService, public profService: ProfDashboardService) { }
+  constructor(public route: Router, public loginService: LoginService, public profService: ProfDashboardService, public headerService: HeaderService) {}
+
 
   ngOnInit() {
 
--- frontend/cams/src/app/shared/past-projects/past-projects.component.ts
+++ frontend/cams/src/app/shared/past-projects/past-projects.component.ts
@@ -5,21 +5,23 @@ import { AssessmentService } from 'src/app/services/assessment.service';
 import { LoginService } from 'src/app/services/login.service';
 import { ProfDashboardService } from 'src/app/services/prof-dashboard.service';
 import { AssessmentDisplay } from 'src/app/AssessmentDisplay';
-
 import { ProjectService } from 'src/app/services/project.service';
 
 @Component({
-  selector: 'app-projects',
-  templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.css']
+  selector: 'app-past-projects',
+  templateUrl: './past-projects.component.html',
+  styleUrls: ['./past-projects.component.css']
 })
-export class ProjectsComponent implements OnInit {
+export class PastProjectsComponent implements OnInit {
 
   user: string | undefined
-  currentAssessments: AssessmentDisplay[] = []
+  pastAssessments: AssessmentDisplay[] = []
+  pS:ProjectService
+
 
 
   constructor(private router: Router, public profDashService: ProfDashboardService, public assessmentService:AssessmentService, loginService:LoginService , public auth:AuthService, public projectService: ProjectService) {
+    this.pS = projectService
     this.user = loginService.email
     this.auth.user$.subscribe(res => {
       this.user = res!.email
@@ -28,17 +30,17 @@ export class ProjectsComponent implements OnInit {
 
   ngOnInit(): void {
     // console.log('what is this? ', this.user)
-    this.assessmentService.getCurrentAssessmentsbyProf(this.user!).subscribe((res: any)=>{
+    this.assessmentService.getPastAssessmentsbyProf(this.user!).subscribe((res: any)=>{
       // console.log('hit: ', res)
-      this.currentAssessments = res
-      this.currentAssessments.sort((a, b) => a.assessment_id - b.assessment_id)
+      this.pastAssessments = res
+      this.pastAssessments.sort((a, b) => a.assessment_id - b.assessment_id)
     }
     )
     this.getDisplayInfo()
   }
 
   getDisplayInfo(){
-    // console.log(this.currentAssessments)
+    // console.log(this.pastAssessments)
   }
 
   assessments(project:AssessmentDisplay) {
